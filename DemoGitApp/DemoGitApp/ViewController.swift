821a79fc72fdc3e44610997b980b211ab49729ae
Anup Samanta <anup.samanta@agnicient.com>
2018-01-15 12:54:30 +0530

    Add Alert Controller

--- DemoGitApp/DemoGitApp/ViewController.swift
+++ DemoGitApp/DemoGitApp/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
